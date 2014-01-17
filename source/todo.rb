822801413ce4b3914cc94ed78a91615e89a9369f
Elliot Chalfant & Matthew Higgins <pair+epchalfant.mh120888@devbootcamp.com>
2014-01-16 20:20:41 -0800

    Elliots refactor

--- source/todo.rb
+++ source/todo.rb
@@ -1,36 +1,10 @@
-# What classes do you need?
-
-=begin
-item class - something that holds the todo item
-              -initialize
-              -content
-              -done/completed
-              -id
-              -mark something as done
-              -priority
-list class - an aggregate of items;
-              -remove item from list
-              -add item from list
-              -print out list/ highest priority first
-              -length
-              -sort based on priority
-              -list name
-              -done?
-              -data structure - hash
-
-read/write module
-              -grab csv
-              -write csv
-
-=end
 require 'csv'
 
 class Item
   attr_accessor :done
-  attr_reader :content, :priority
-  def initialize(content, priority=3)
+  attr_reader :content
+  def initialize(content)
     @content = content
-    @priority = priority
     @done = false
   end
   def found_item?(string)
@@ -41,11 +15,11 @@ end
 class List # controller?
   attr_reader :list
   def initialize
-    @list = Array.new
-    @list = Model.pull_file(self)
+    @list = Model.pull_file
   end
-  def add(content, priority = 3)
-    @list << Item.new(content, priority)
+  def add(content)
+    @list << Item.new(content)
+    Model.push_file(@list.to_a) # get into array
   end
   def remove_done
     @list.delete_if { |item| item.done }
@@ -56,56 +30,33 @@ class List # controller?
   def mark_as_complete(string)
     @list.each { |item| item.done = true if item.found_item?(string) }
   end
-  def sort_by_priority!
-    @list.sort! { |item1, item2| item2.priority <=> item1.priority }
-  end
   def display_list
-    sort_by_priority!
-    @list.each {|item| puts "#{item.priority} - #{item.content}" }
+    @list.each {|item| puts "#{item.content}" }
+  end
+  def save
+    Model.push_file(@list.to_a) #get into array
   end
 end
 
 class Model
-  def self.pull_file(list_object)
-    CSV.foreach("todo.csv") { |row| list_object.list << row }
-    list_object.list.flatten!
-    make_item_objects(list_object)
+  def self.pull_file
+    file_array = Array.new
+    CSV.foreach("todo.csv") { |row| file_array << row }
+    file_array.flatten!
+    make_item_objects(file_array)
   end
-  def self.make_item_objects(list_object)
-    list_object.list.map {|item| Item.new(item)}
+  def self.make_item_objects(file_array)
+    file_array.map {|item| Item.new(item)}
+  end
+  def self.push_file(file_array)
+    CSV.open("todo.csv") do |csv|
+      file_array.each do |list_object|
+        csv << list_object
+      end
+    end
   end
 end
 
-
-list = List.new
-p list.list
-# list.add("item 1", 3)
-# list.add("item 2")
-
-# Model.pull_file(list)
-
-#   list.send ARGV[0].to_sym, *ARGV[1], *ARGV[2].to_i
-
-# list.display_list
-
-# p list.list
-
-# p "--------------"
-# item = Item.new("this is an item", 3)
-# # p item.found_item?("this is an item") == true
-# list.remove_item("item 1")
-# p list.list
-# p "--------------"
-# list.mark_as_complete("item 2")
-# p list.list
-# p list.remove_done
-# p "--------------"
-# list.add("Really important", 5)
-# list.add("Kind of important", 3)
-# list.add("Not important", 1)
-# list.add("indifferent", 2)
-# # list.sort_by_priority!
-# list.display_list
 
 
 # Remember, there are four high-level responsibilities, each of which have multiple sub-responsibilities:
