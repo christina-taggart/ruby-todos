b09a6b8b7b3ebe7ece09f8e5b7224c535e4e7a63
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-16 16:44:12 -0800

    Refactored by separating the viewer tasks from the List class and adds them to a new class ListInterface

--- source/todo.rb
+++ source/todo.rb
@@ -25,49 +25,38 @@
 # Note that (4) is where the essence of your application lives.
 # Pretty much every application in the universe has some version of responsibilities (1), (2), and (3).
 
-
-
 require 'csv'
+
 class List
   attr_accessor :tasklist
-  def initialize
-    @tasklist = CSV.read("todo.csv").map{ |task| Task.new( task[0...-1].join, (task[-1].strip == 'true') ) }
+  def initialize(tasklist)
+    @tasklist = tasklist
   end
 
-  def add(new_task = ARGV[1..-1].join(" "))
+  def add(new_task)
     system("clear")
     @tasklist << Task.new(new_task)
-    write_to_file
     print
   end
 
-  def delete(task_num = ARGV[1..-1].join.to_i)
+  def delete(task_num)
     system("clear")
     puts "Deleted '#{@tasklist[task_num-1].task}' from your TODO list"
     @tasklist.delete_at(task_num-1)
-    write_to_file
     print
   end
 
-  def complete_task(task_num = ARGV[1..-1].join.to_i)
+  def complete_task(task_num)
     system("clear")
     @tasklist[task_num - 1].done = true
-    write_to_file
     puts "'#{@tasklist[task_num-1].task}' has been marked Complete."
     print
-  end
-
-  def write_to_file
-    CSV.open('todo.csv', 'w') do |csv|
-      @tasklist.each{ |item| csv << [item.task, item.done] }
-    end
   end
 
   def print
     puts "\nThings to do:\n\n"
     @tasklist.each_with_index { |task, index| puts "#{(index+1).to_s.rjust(2)}.   #{task.done ? '[X]' : '[ ]'}   #{task.task}" }
   end
-
 end
 
 class Task
@@ -78,6 +67,42 @@ class Task
   end
 end
 
+# Separate List (our model/controller) from our viewer with
+# a new object ListInterface:
+class ListInterface
+  def initialize
+    @command = ARGV[0]
+    @argument = ARGV[1..-1]
+  end
+
+  def run!
+    user_list = List.new(open_file)
+    case @command
+    when "add"
+      user_list.add(@argument.join(" "))
+    when "delete"
+      user_list.delete(@argument.join.to_i)
+    when "complete_task"
+      user_list.complete_task(@argument.join.to_i)
+    when "print"
+      user_list.print
+    else
+      puts "Invalid command."
+    end
+    write_to_file(user_list)
+  end
+
+  def open_file
+    CSV.read("todo.csv").map{ |task| Task.new( task[0...-1].join, (task[-1].strip == 'true') ) }
+  end
+
+  def write_to_file(list)
+    CSV.open('todo.csv', 'w') do |csv|
+      list.tasklist.each{ |item| csv << [item.task, item.done] }
+    end
+  end
+end
+
 #-------Driver---Code------
-rocky = List.new
-rocky.send(ARGV[0])
+viewer = ListInterface.new
+viewer.run!
